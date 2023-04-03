10e04d6790ebc9dba58ff0ed0c86a28038ca4e1b
Dalison Dieyson Rocha <dalisonfatality@gmail.com>
2023-04-03 17:10:58 -0300

    new update
    Adicionado efeito accordion, alinhamento de backgrond logo e botão fechar tudo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let accordions = document.querySelectorAll("button.accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  };
+}
+function closeAll() {
+  for (let i = 0; i < accordions.length; i++) {
+    accordions[i].classList.remove("active");
+    accordions[i].nextElementSibling.classList.remove("show");
+  }
+}
