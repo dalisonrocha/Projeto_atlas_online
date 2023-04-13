aba12097f379f4ca13d51cf73feb0f9af2eb2290
Dalison Dieyson Rocha <dalisonfatality@gmail.com>
2023-04-13 17:31:49 -0300

    Html changes
    add images and new names

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ let accordions = document.querySelectorAll("button.accordion");
 for (let i = 0; i < accordions.length; i++) {
   accordions[i].onclick = function() {
     this.classList.toggle("active");
+    this.classList.toggle("opacity");
     this.nextElementSibling.classList.toggle("show");
+    this.nextElementSibling.classList.toggle("opacity");
   };
 }
 function closeAll() {
